bec333d73484fc1f1e8cd794c76aada1f0e848da
Tomáš Zamba <tomas.zamba@odyzeo.com>
2026-03-27 19:53:41 +0100

    Add manually manifest from @vite-pwa/nuxt due ssr: false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,7 @@ export default defineNuxtConfig({
                 {rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg'},
                 {rel: 'shortcut icon', href: '/favicon/favicon.ico'},
                 {rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png'},
+                {rel: 'manifest', href: '/manifest.webmanifest'}
             ],
         },
     },
